eda3a06b25ca3aed4bb1463f9f6b03cda9444230
JosephGhan <47993853+JosephGhan@users.noreply.github.com>
2020-04-23 19:28:45 -0500

    Start

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                SphereCollider sp = hit.collider as SphereCollider;
+                if (sp != null)
+                {
+                    Destroy(sp.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform groundCheck;
+    public CharacterController controller;
+    public float gravity = -9.81f;
+    public float speed = 12f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public AudioSource newAudio;
+
+    Vector3 velocity;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(!newAudio.isPlaying)
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                newAudio.Play();
+                print("Started loop");
+            }
+        }
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 && newAudio.isPlaying)
+        {
+            newAudio.Pause();
+            print("Stopped loop");
+        }
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
